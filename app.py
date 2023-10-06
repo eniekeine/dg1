c6079084b93a1007404c6720e6270709fdbc1ac1
eniekeine <eniekeine@gmail.com>
2023-10-06 03:58:31 +0000

    app.run() 삭제

--- app.py
+++ app.py
@@ -2,16 +2,11 @@ import sys
 import os
 from flask import Flask, render_template
 
-PORT = os.environ["DG1_PORT_NUMBER"]
 app = Flask(__name__)
 basedir = os.path.abspath(__file__)
 print(PORT)
-
 
 @app.route('/')
 def current_time():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(port=PORT)
