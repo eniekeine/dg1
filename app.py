7b4856d9e1124f5d77a8d62bb5b9e9af84d8c9f4
eniekeine <eniekeine@gmail.com>
2023-10-12 13:17:05 +0900

    스태틱 파일 전송 라우팅 규칙 추가

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 basedir = os.path.abspath(__file__)
@@ -9,3 +9,6 @@ basedir = os.path.abspath(__file__)
 def current_time():
     return render_template('index.html')
 
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
