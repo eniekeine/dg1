1d7cb2cf2efd62a4cba3c9e81b9240a60c56675a
eniekeine <eniekeine@gmail.com>
2023-10-18 20:02:57 +0900

    이전 커밋의 내용과 동일

--- static/dist/js/index.js
+++ static/dist/js/index.js
@@ -168,6 +168,88 @@
         }
     }
 
+    /* EXPANDER MENU */
+    let navState = 'close';
+    const elemNavList = document.querySelector('.nav__list');
+    const toggle = document.getElementById('nav-toggle');
+    const navbar = document.getElementById('navbar');
+    const bodypadding = document.getElementById("body-pd");
+    const elemBtnNewChatText = document.querySelector(".btn-new-chat-text");
+
+    // 채팅 목록에 변화가 있을 경우
+    document.addEventListener('chatsUpdated', e => {
+        // 사이드바의 내용을 그에 맞춰 업데이트 할 것
+        updateSidebar();
+    });
+
+    toggle.addEventListener('click', ()=>{
+        if( navState == 'close' ) navState = 'open';
+        else navState = 'close';
+        navbar.classList.toggle('expander');
+        bodypadding.classList.toggle('body-pd');
+        showButtons(navState == 'open');
+    });
+
+    function showButtons(show)
+    {
+        let removeButtons = document.getElementsByClassName('btn-remove-chat');
+        for( let i = 0; i < removeButtons.length; ++i )
+        {
+            if( show ) removeButtons[i].classList.remove('hidden');
+            else removeButtons[i].classList.add('hidden');
+        }
+        let editButtons = document.getElementsByClassName('btn-edit-title');
+        for( let i = 0; i < editButtons.length; ++i )
+        {
+            if( show ) editButtons[i].classList.remove('hidden');
+            else editButtons[i].classList.add('hidden');
+        }
+        if( show ) elemBtnNewChatText.classList.remove('hidden');
+        else elemBtnNewChatText.classList.add('hidden');
+    }
+
+    // 사이드바에 채팅 목록 요소를 추가하는 함수
+    function updateSidebar()
+    {
+        elemNavList.textContent = "";
+        for(let i = 0; i < chats.length; ++i )
+        {
+            let nl = createNavLink(chats[i]);
+            elemNavList.appendChild(nl);
+        }
+        showButtons(navState == 'open');
+    }
+
+    // 히스토리에 표시되는 채팅 목록 요소를 만드는 함수
+    function createNavLink(chatModel) {
+        const templateNavLink = document.querySelector('#template-nav-link').content;
+        const clone = document.importNode(templateNavLink, true); // This creates a deep clone of the template content.
+        const anchor = clone.querySelector('.nav__link');
+        const elemBtnRemoveChat = clone.querySelector('.btn-remove-chat');
+        const elemBtnEditTitle = clone.querySelector('.btn-edit-title');
+        const span = anchor.querySelector('span');
+        span.textContent = chatModel.title;
+        anchor.addEventListener('mousedown', event => {
+            selectChat(chatModel);
+        });
+        elemBtnRemoveChat.addEventListener('mousedown', event =>{
+            if( currChat == chatModel )
+            {
+                console.log("currChat removed");
+                selectChat(null);
+            }
+            let index = chats.indexOf(chatModel);
+            chats.splice(index, 1);
+            updateSidebar();
+        });
+        elemBtnEditTitle.addEventListener('mousedown', event => {
+            console.log("edit");
+        });
+        if ( chatModel == currChat )
+            anchor.classList.add('active');
+        return clone;
+    }
+
     /*
         파일이름 : index.js
         생성일 : 2023년 10월 16일 이경근이 만들었습니다.
