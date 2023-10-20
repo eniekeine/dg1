4aa708b3e18515e1fe8901458a4403345875fdb8
eniekeine <eniekeine@gmail.com>
2023-10-20 11:40:59 +0900

    예외 처리 추가 - OpenAI 서버에 문제가 있을 경우

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # 파일이름 : app.py 
 # 설명 : 플라스크 백엔드 서버의 라우팅을 정의하는 파일입니다.
-from flask import Flask, render_template, send_from_directory, jsonify, request, Response, stream_with_context
+import os
+from flask import Flask, render_template, send_from_directory, jsonify, request, Response, stream_with_context, make_response
 import openai
 import time
 
@@ -45,34 +46,8 @@ def text_query():
 def serve_static(path):
     return send_from_directory('static', path)
 
-
-# This function simulates the streaming behavior of OpenAI's API
-def simulated_openai_streamed_chat():
-    chats = [
-        "Hello, how can I help you?",
-        "I can provide information on various topics.",
-        "Do you have any questions?"
-    ]
-    for chat in chats:
-        time.sleep(2)  # Simulating a delay for each chat chunk
-        yield chat
-
-@app.route('/streamed-chat', methods=['POST'])
-def streamed_chat():
-    def generate():
-        for chat in simulated_openai_streamed_chat():
-            yield chat + '\n'  # Sending each chat as a separate chunk
-
-    return Response(stream_with_context(generate()), content_type='text/plain')
-
-@app.route('/text-stream-query', methods=['POST'])
-def text_stream_query():
-    def generate():
-        # i.e. data = { "queryText" : "1+1은 2다"};
-        data = request.json
-        print(data)
-        # OpenAI API 서버에 요청을 보냅니다.
-        # response : Dictionary 사전 객체
+def is_openapi_server_okay():
+    try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -80,20 +55,64 @@ def text_stream_query():
                     # TODO : 프롬프트
                     {"role": "system", "content": "You are a helpful assistant."},
                     # 사용자의 요청
-                    {"role": "user", "content": data["queryText"]},
-                ],
-            stream=True
+                    {"role": "user", "content": "1이라고 말해봐"},
+                ]
         )
-        # create variables to collect the stream of chunks
-        collected_chunks = []
-        # iterate through the stream of events
-        for chunk in response:
-            collected_chunks.append(chunk)  # save the event response
-            chunk_message = chunk['choices'][0]['delta']  # extract the message
-            content = chunk_message.content
-            if len(content) == 0:
-                continue
-            yield content  # Sending each chat as a separate chunk
-            # print(f"sent {content}")  # print the delay and text
-    
-    return Response(stream_with_context(generate()), content_type='text')
+        return True
+    except openai.error.APIError as e:
+        return False
+    except openai.error.APIConnectionError as e:
+        return False
+    except openai.error.RateLimitError as e:
+        return False
+
+@app.route('/text-stream-query', methods=['POST'])
+def text_stream_query():
+    print(" ==================== text_stream_query ====================")
+    if is_openapi_server_okay() == False:
+        return make_response("OpenAI 서버가 다운되었습니다.", 500)
+    def generate():
+        # i.e. data = { "queryText" : "1+1은 2다"};
+        data = request.json
+        print(data)
+        # OpenAI API 서버에 요청을 보냅니다.
+        # response : Dictionary 사전 객체
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                        # TODO : 앞에서 대화한 내용 추가
+                        # TODO : 프롬프트
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        # 사용자의 요청
+                        {"role": "user", "content": data["queryText"]},
+                    ],
+                stream=True
+            )
+            # create variables to collect the stream of chunks
+            collected_chunks = []
+            # iterate through the stream of events
+            for chunk in response:
+                collected_chunks.append(chunk)  # save the event response
+                chunk_message = chunk['choices'][0]['delta']  # extract the message
+                content = chunk_message.content
+                if len(content) == 0:
+                    time.sleep(1)
+                    continue
+                yield content  # Sending each chat as a separate chunk
+                print(f"sent {content}")  # print the delay and text
+        except openai.error.APIError as e:
+            print(f"OpenAI API returned an API Error: {e}")
+            yield "!! 답변을 생성하던 중 에러가 발생했습니다."
+        except openai.error.APIConnectionError as e:
+            print(f"Failed to connect to OpenAI API: {e}")
+            yield "!! 답변을 생성하던 중 에러가 발생했습니다."
+        except openai.error.RateLimitError as e:
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            yield "!! 답변을 생성하던 중 에러가 발생했습니다."
+
+    return Response(stream_with_context(generate()))
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
